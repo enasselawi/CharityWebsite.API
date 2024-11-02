d7509d464cc1264404f706168bb9dd561d39561f
enas selawi <enasselawi6@gmail.com>
2024-11-02 03:58:15 +0300

    yyy

--- CharityWebsite.Infra/Repository/TestimonialRepository.cs
+++ CharityWebsite.Infra/Repository/TestimonialRepository.cs
@@ -10,7 +10,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace CharityWebsite.Infra.Repository
-{//hgfd
+{//hgfduuuuuuuuuuuuuuuuuuuuuuuuuu
     public class TestimonialRepository : ITestimonialRepository
     {
         private readonly IDbContext dBContext;
